87647d6ce9e8d73b48c9540ab60e9f16656e0651
codejss <josue1789ss@gmail.com>
2022-10-08 14:18:11 -0600

    added PetTypeService. closes #29

---
+++ pet-clinic-data/src/main/java/com/codejss/petclinic/services/PetTypeService.java
@@ -0,0 +1,9 @@
+package com.codejss.petclinic.services;
+
+import com.codejss.petclinic.models.PetType;
+
+/**
+ * @author codejss
+ */
+public interface PetTypeService extends CrudService<PetType, Long>{
+}
