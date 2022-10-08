d3f50b78d6ed3b9948dd4888ccd92ef40afc3462
codejss <josue1789ss@gmail.com>
2022-10-08 13:59:01 -0600

    added Specialities, added to vet closes #27

---
+++ pet-clinic-data/src/main/java/com/codejss/petclinic/models/Speciality.java
@@ -0,0 +1,17 @@
+package com.codejss.petclinic.models;
+
+/**
+ * @author codejss
+ */
+public class Speciality extends BaseEntity{
+
+    private String description;
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
