ea20b43fb5e6186898e921285cf0d0d11496fe21
codejss <josue1789ss@gmail.com>
2022-10-08 20:02:00 -0600

    added pets and contact info to owners on startup. closes #33

--- pet-clinic-data/src/main/java/com/codejss/petclinic/services/map/OwnerServiceMap.java
+++ pet-clinic-data/src/main/java/com/codejss/petclinic/services/map/OwnerServiceMap.java
@@ -1,7 +1,10 @@
 package com.codejss.petclinic.services.map;
 
 import com.codejss.petclinic.models.Owner;
+import com.codejss.petclinic.models.Pet;
 import com.codejss.petclinic.services.OwnerService;
+import com.codejss.petclinic.services.PetService;
+import com.codejss.petclinic.services.PetTypeService;
 import org.springframework.stereotype.Service;
 
 import java.util.Set;
@@ -11,6 +14,14 @@ import java.util.Set;
  */
 @Service
 public class OwnerServiceMap extends AbstractMapService<Owner, Long> implements OwnerService {
+
+    private final PetTypeService petTypeService;
+    private final PetService petService;
+
+    public OwnerServiceMap(PetTypeService petTypeService, PetService petService) {
+        this.petTypeService = petTypeService;
+        this.petService = petService;
+    }
 
     @Override
     public Set<Owner> findAll() {
@@ -29,7 +40,28 @@ public class OwnerServiceMap extends AbstractMapService<Owner, Long> implements 
 
     @Override
     public Owner save(Owner object) {
-        return super.save(object);
+
+        if (object != null) {
+            if (object.getPets() != null) {
+                object.getPets().forEach(pet->{
+                    if (pet.getPetType() != null) {
+                        if (pet.getPetType().getId() == null) {
+                            pet.setPetType(petTypeService.save(pet.getPetType()));
+                        }
+                    }else {
+                        throw new RuntimeException("Pet Type s required");
+                    }
+                    if (pet.getId() == null) {
+                        Pet savedPet = petService.save(pet);
+                        pet.setId(savedPet.getId());
+                    }
+
+                });
+            }
+            return super.save(object);
+        } else {
+            return null;
+        }
     }
 
     @Override
--- pet-clinic-web2/src/main/java/com/codejss/petclinic/boostrap/DataLoader.java
+++ pet-clinic-web2/src/main/java/com/codejss/petclinic/boostrap/DataLoader.java
@@ -2,6 +2,7 @@ package com.codejss.petclinic.boostrap;
 
 
 import com.codejss.petclinic.models.Owner;
+import com.codejss.petclinic.models.Pet;
 import com.codejss.petclinic.models.PetType;
 import com.codejss.petclinic.models.Vet;
 import com.codejss.petclinic.services.OwnerService;
@@ -9,6 +10,8 @@ import com.codejss.petclinic.services.PetTypeService;
 import com.codejss.petclinic.services.VetService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
+
+import java.time.LocalDate;
 
 /**
  * @author codejss
@@ -32,23 +35,43 @@ public class DataLoader implements CommandLineRunner {
 
         PetType dog = new PetType();
         dog.setPetName("Dog");
-        petTypeService.save(dog);
+        var savedDog = petTypeService.save(dog);
 
         PetType cat = new PetType();
         cat.setPetName("cat");
-        petTypeService.save(cat);
+        var savedCat = petTypeService.save(cat);
 
         System.out.println("Loader PetTypes.........");
 
         Owner owner1 = new Owner();
         owner1.setFirstName("Michael");
         owner1.setLastName("Weston");
+        owner1.setAddress("123 ST Carolina");
+        owner1.setCity("Miami");
+        owner1.setTelephone("23457532");
+
+        Pet mickaelPet = new Pet();
+        mickaelPet.setName("Negrito");
+        mickaelPet.setPetType(savedDog);
+        mickaelPet.setBirthDate(LocalDate.now());
+        mickaelPet.setOwner(owner1);
+        owner1.getPets().add(mickaelPet);
 
         ownerService.save(owner1);
 
         Owner owner2 = new Owner();
         owner2.setFirstName("Fiona");
         owner2.setLastName("Glennane");
+        owner2.setAddress("123 ST Carolina");
+        owner2.setCity("Miami");
+        owner2.setTelephone("234767532");
+
+        var fionaPet = new Pet();
+        fionaPet.setName("Meo");
+        fionaPet.setOwner(owner2);
+        fionaPet.setPetType(savedCat);
+        fionaPet.setBirthDate(LocalDate.now());
+        owner2.getPets().add(fionaPet);
 
         ownerService.save(owner2);
 
